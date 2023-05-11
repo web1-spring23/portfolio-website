3de3df1a44b42efaf7e7fa5bfb3eefac526d1d46
englisj3 <englisj3@tcnj.edu>
2023-05-11 14:08:23 -0400

    button-fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@ console.log("hello world!");
 
 $('#menu-button').click(function(){
     $("#overlay-menu-container").fadeIn();
+    $("body").addClass("no-scroll");
 });
 
 $('#close-menu').click(function(){
-    $('#overlay-menu-container').fadeOut()
+    $('#overlay-menu-container').fadeOut();
+    $("body").removeClass("no-scroll");
 });
